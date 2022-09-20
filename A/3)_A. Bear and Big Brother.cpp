27d3a9c8e7dab2ec59232da7903c907a969d1efe
Kareem Ahmed <100070833+Ka7032799@users.noreply.github.com>
2022-09-20 20:30:56 +0200

    u

---
+++ A/3)_A. Bear and Big Brother.cpp
@@ -0,0 +1,27 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+ 
+int main()
+{
+int a,b;
+//a*3 //b*2
+cin>>a>>b;
+int cnt=0;
+ 
+while(true){
+    a*=3;
+    b*=2;
+     cnt++;
+    if(a>b){break;}
+  
+   
+}
+ 
+cout<<cnt;
+   
+}
+
+
